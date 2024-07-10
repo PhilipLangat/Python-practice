92874d9588e2490954f21339c964c2cf9ea93ac3
PhilipLangat <langatp469@gmail.com>
2024-07-10 12:22:36 +0300

    commit message

---
+++ loops.py
@@ -0,0 +1,21 @@
+#Loops are also known as iteration control structures
+
+#Print numbers 0 - 5 without a loop
+'''
+print("0")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+'''
+count = 0
+while count<=5:
+    print(count)
+    count = count+1
+    
+    
+count = 100
+while count>0:
+    print(count)
+    count = count-1
---
+++ selection.py
@@ -0,0 +1,20 @@
+"""
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+    """
+   
+score = int(input("Enter your score: "))
+if score >= 90:
+    print("Grade A")
+elif score >=80:
+    print("Grade B")
+elif score>=70:
+    print("Grade C")
+elif score>=60:
+    print("Grade D")
+else:
+    print("You have failed")
