0fabfaed5fe98c3f005907c1633472d7f18c4376
PhilipLangat <langatp469@gmail.com>
2024-07-10 13:12:27 +0300

    Commit message

---
+++ break.py
@@ -0,0 +1,8 @@
+#This is for mastering break statements in loops
+count = 100
+while count>=0:
+    print(count)
+    if count == 90:
+        print("Yaay, we found it!")
+        break
+    count = count-1
